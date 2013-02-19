ea365cb79ecf17c9fec639954f3f5e7e848cfe4a
Lei Zhang <zhang.lei.fly@gmail.com>
2013-02-19 13:23:31 +0800

    init project

---
+++ setup.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+        name='jblog',
+        version='1.0',
+        description="this is my blog program",
+        author="Jeffrey4l",
+        author_email="zhang.lei.fly@gmail.com",
+        packages=find_packages(),
+        install_requires=['bottle'],
+        )
+
